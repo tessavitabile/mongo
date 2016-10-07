fc7e1f8c97e830491368c26695cc01151fcd91a1
Tess Avitabile <tess.avitabile@mongodb.com>
2016-10-07 16:48:51 -0400

    in progress

--- jstests/core/list_collections1.js
+++ jstests/core/list_collections1.js
@@ -32,6 +32,7 @@
     assert.eq('object', typeof(collObj.options));
     assert.eq('collection', collObj.type, tojson(collObj));
     assert.eq(false, collObj.info.readOnly, tojson(collObj));
+    assert.eq('object', typeof(collObj.idIndex));
 
     //
     // Test basic usage with DBCommandCursor.
